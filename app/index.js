ae8160f88d2009b2af24926c1c587221756f05ef
Josue <josueelias94@gmail.com>
2017-02-11 17:17:42 -0200

    udemy es6 api react

--- app/index.js
+++ app/index.js
@@ -1,1 +1,54 @@
-console.log("Hello world!");
+// import {students, total} from './students';
+// import multiply from './calculator';
+
+// console.log( multiply(3, 5));
+// import Entity from './entity';
+
+// class Hobbit extends Entity {
+//   constructor(name, height){
+//     super(name, height);
+//   }
+//   greet()
+//   {
+//     console.log(`Eu sou ${this.name} do Condado!`);
+//   }
+// }
+
+// let Frodo = new Hobbit("Frodo Beggins", 4.5);
+// Frodo.greet();
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+const api_key = '003c2c4e49c06b0cd185d6968d0535c3';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+      this.state = {
+        city: 'Sao Paulo'
+    };
+  }
+
+componentDidMount()
+{
+  this.grabWeather(this.state.city);
+}
+
+grabWeather(city){
+  fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${api_key}&q=${city}`)
+  .then(response => response.json())
+  .then(json => {
+    this.setState({description: json.weather[0].description})
+});}
+render() {
+    return (
+      <div>
+        <h1>A cidade do baguio {this.state.city}</h1>
+        <h2>{this.state.description}</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App></App>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   entry: ['./app/index.js'],
   output: {
-    path: './build',
+    path: __dirname + './build',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders:[{
+      loader: 'babel-loader',
+      test:  /\.js$/,
+      exclude: /node_modules/
+  }]
+  },
+  devServer: {
+    port: 3001,
+    contentBase: './build',
+    inline: true
   }
 }
